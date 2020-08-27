bcd095b8f1b05965144ecc1849dcc97d3103a29c
Soulballad <soda931vzr@163.com>
2020-08-27 23:36:57 +0800

    add

---
+++ usage-pattern/pattern-08-flyweight/src/main/java/com/soulballad/usage/p1/general/IFlyweight.java
@@ -0,0 +1,6 @@
+package com.soulballad.usage.p1.general;
+
+// 抽象享元角色
+public interface IFlyweight {
+    void operation(String extrinsicState);
+}
---
+++ usage-pattern/pattern-08-flyweight/src/main/java/com/soulballad/usage/p2/ticket/ITicket.java
@@ -0,0 +1,5 @@
+package com.soulballad.usage.p2.ticket;
+
+public interface ITicket {
+    void showInfo(String bunk);
+}
