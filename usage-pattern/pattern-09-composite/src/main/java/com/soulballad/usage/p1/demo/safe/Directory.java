6a4256bb646d3052e25df7b872f69903983f2620
Soulballad <soda931vzr@163.com>
2020-08-28 22:25:48 +0800

    add

---
+++ usage-pattern/pattern-09-composite/src/main/java/com/soulballad/usage/p1/demo/safe/Directory.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.p1.demo.safe;
+
+public abstract class Directory {
+
+    protected String name;
+
+    public Directory(String name) {
+        this.name = name;
+    }
+
+    public abstract void show();
+
+}
