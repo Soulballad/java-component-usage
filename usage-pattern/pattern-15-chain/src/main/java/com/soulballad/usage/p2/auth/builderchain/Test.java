90c196078cdf6d813197e7fa343d1fa627b70f33
Soulballad <soda931vzr@163.com>
2020-08-30 21:18:29 +0800

    add

---
+++ usage-pattern/pattern-15-chain/src/main/java/com/soulballad/usage/p2/auth/builderchain/Test.java
@@ -0,0 +1,11 @@
+package com.soulballad.usage.p2.auth.builderchain;
+
+/**
+ * Created by Tom.
+ */
+public class Test {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberService();
+        memberService.login("tom","666");
+    }
+}
---
+++ usage-pattern/pattern-15-chain/src/main/java/com/soulballad/usage/p2/auth/optimiaze/LoginHandler.java
@@ -0,0 +1,14 @@
+package com.soulballad.usage.p2.auth.optimiaze;
+
+import com.gupaoedu.vip.pattern.chain.auth.Member;
+
+/**
+ * Created by Tom.
+ */
+public class LoginHandler extends Handler {
+    public void doHandler(Member member) {
+        System.out.println("登录成功！");
+        member.setRoleName("管理员");
+        next.doHandler(member);
+    }
+}
