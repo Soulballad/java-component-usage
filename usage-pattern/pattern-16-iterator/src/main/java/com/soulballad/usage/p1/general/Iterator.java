5b4eebd6c316e5a79e25cf2fcc04cb39f2a366cc
Soulballad <soda931vzr@163.com>
2020-08-30 21:20:03 +0800

    add

---
+++ usage-pattern/pattern-16-iterator/src/main/java/com/soulballad/usage/p1/general/Iterator.java
@@ -0,0 +1,9 @@
+package com.soulballad.usage.p1.general;
+
+//抽象迭代器
+public interface Iterator<E> {
+
+    E next();
+
+    boolean hasNext();
+}
---
+++ usage-pattern/pattern-16-iterator/src/main/java/com/soulballad/usage/p2/course/Iterator.java
@@ -0,0 +1,9 @@
+package com.soulballad.usage.p2.course;
+
+/**
+ * Created by Tom.
+ */
+public interface Iterator<E> {
+    E next();
+    boolean hasNext();
+}
