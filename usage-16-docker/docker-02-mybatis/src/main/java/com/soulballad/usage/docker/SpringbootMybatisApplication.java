2c40eec713e7199efc67c3d82b66dc605b096319
soulballad <13037106803@163.com>
2022-07-15 13:11:41 +0800

    add docker

---
+++ usage-16-docker/docker-02-mybatis/src/main/java/com/soulballad/usage/docker/SpringbootMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.soulballad.usage.docker.SpringbootMybatisApplication.class, args);
+	}
+
+}
